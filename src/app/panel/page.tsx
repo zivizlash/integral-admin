c310d4fe7789a2c60125b5135d7a8c4d240e27dd
Yaroslav Yaroslavtsev <alienmonsterrus@gmail.com>
2024-09-22 18:13:39 +0700

    forcing dark mode

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -158,8 +158,8 @@ export default function Page() {
 
           return {
             categoryId,
-            oldValue: oldChange!.value,
-            newValue: newChange!.value
+            oldValue: oldChange?.value ?? "",
+            newValue: newChange?.value ?? ""
           };
         }
       );
