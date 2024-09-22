9618705deff76aa6234aaa851f482b043a0f2d6c
Yaroslav Yaroslavtsev <alienmonsterrus@gmail.com>
2024-09-22 18:05:03 +0700

    try force dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./components/providers";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["cyrillic", "latin"] });
 
@@ -12,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="">
+    <html lang="en" className="dark">
       <body className={`${inter.className} antialised`}>
         <div className="container min-h-screen mx-auto max-w-screen-lg">
           <Providers>
